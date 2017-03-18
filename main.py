c302ddfc64d077b9137a0ba6a85a4f86f5a82742
Gabe Hodge <gdhodge@umich.edu>
2017-03-18 16:17:11 -0400

    added some structure to main

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import sys
+
+
 def main():
     # Add option to take in
     classifier_opts  = sys.argv[1]  
     vect_opts = sys.argv[2]
-    train(clasifier_opts, vect_opts)
+    train(classifier_opts, vect_opts)
     test()
     
 def train(classifier_opts, vect_opts):
 
     """
-    Load the data
+    Load the IDs
     """
-    TrainID = []
-    TestID = []
-    
-    if clasifier_opts == "naive_bayes":
-        
-    else:
-        vec = tf_idf()       
-    
- 
-    """
-    Train the classifier
-    """
-    
+    train_ID = []
+    test_ID = []
 
-    """
-    Test
-    """
-  
+    # read training data
+
+    if classifier_opts == "naive_bayes":
+        pass
+    else:  # Vectorization
+        pass
+        # train_vecs, test_vec = vectorize(TrainID, TestID, vect_opts)
+        # classification method (cosine sim, machine learning, etc)
+
+
+def test():
+    pass
+
+
 if __name__ == "__main__":
-  main()
+    main()
