0327c4a2317aac84c3118073b42096cb28c155cd
Hazrael <zharry@umich.edu>
2017-03-18 15:31:55 -0400

    Changed code file structure

--- load_from_db/load_from_db.py
+++ load_from_db/load_from_db.py
@@ -1,5 +1,4 @@
 import sqlite3
 conn = sqlite3.connect('mxm_dataset.db')
 c = conn.cursor()
-for row in c.execute('SELECT * FROM Lyrics LIMIT 5'):
-        print row
+c.execute('SELECT * FROM Lyrics LIMIT 5')
