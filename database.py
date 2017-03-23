258430ec59254ec9a41d11810560c540fecff60a
Gabe Hodge <gdhodge@umich.edu>
2017-03-23 18:51:09 -0400

    return string instead of list of tuples

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import sqlite3
 conn = sqlite3.connect('mxm_dataset.db')
 
+
 def get_track(track_id):
     c = conn.cursor()
     c.execute('SELECT word, count FROM Lyrics WHERE track_id="%s"' % track_id)
     words = c.fetchall()
     c.close()
 
-    return words
+    lyrics = []
+    for word, count in words:
+        lyrics.extend([word for i in range(count)])
+    return " ".join(lyrics)
