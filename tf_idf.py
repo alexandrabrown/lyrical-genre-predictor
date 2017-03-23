b2218ce737b3aecb0da617283ce3046a630b4b76
Hazrael <zharry@umich.edu>
2017-03-23 18:32:32 -0400

    tfidf not done yet

--- tf_idf.py
+++ tf_idf.py
@@ -1,15 +1,17 @@
 import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def tf_idf_vectorize():
-    corpus = [
-    'This is the first document.',
-    'This is the second second document.',
-    'And the third one.',
-    'Is this the first document?',
-    ]
+def tf_idf_vectorize(train_lyrics, test_lyrics):
+    # corpus = [
+    # 'This is the first document.',
+    # 'This is the second second document.',
+    # 'And the third one.',
+    # 'Is this the first document?',
+    # ]
+	corpus = train_lyrics + test_lyrics
 
     vectorizer = TfidfVectorizer(min_df=1)
     vectorizer.fit_transform(corpus)
 
-    return vectorizer.fit_transform(corpus).toarray()
+    return vectorizer.fit_transform(corpus)
+	# TODO: Split
