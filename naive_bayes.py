833fe066a7c149be181e86e8865ab2ff412ca230
alexbro <alexbro@umich.edu>
2017-03-25 14:45:46 -0400

    Add Pipeline code

--- naive_bayes.py
+++ naive_bayes.py
@@ -3,17 +3,26 @@
 # sudo pip3 install -U numpy
 # sudo pip3 install -U scipy
 # http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn import datasets
+from sklearn.pipeline import Pipeline
 import sys
 
 
 def main():
-    iris = datasets.load_iris()
-    print(iris)
-    gnb = GaussianNB()
-    y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
-    print(y_pred)
+    # iris = datasets.load_iris()
+    # print(iris)
+    # gnb = GaussianNB()
+    # y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+    # print(y_pred)
+
+
+    text_clf = Pipeline([('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', MultinomialNB()) ])
+    
 
 
 if __name__ == '__main__':
