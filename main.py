cb8f7ad9e8513b8216cbb627bf9e06a91a51645c
alexbro <alexbro@umich.edu>
2017-03-26 14:56:20 -0400

    Restructure main and add in vectorization and naive_bayes classifications

--- main.py
+++ main.py
@@ -3,52 +3,69 @@ import vectorization
 
 
 def main():
-    # Add option to take in
+    
+    """
+    Usage: python3 main.py <naive_bayes><svm> <tfidf>
+    TODO: allow lyrics to be read from an optional filename
+    """
+
     classifier_opts = sys.argv[1]
     vect_opts = sys.argv[2]
-    train(classifier_opts, vect_opts)
-    test()
+    predicted_test_categories, test_truth = classify_songs(classifier_opts, vect_opts)
+    evaluation(predicted_test_categories, test_truth)
 
 
-def train(classifier_opts, vect_opts):
+def classify_songs(classifier_opts, vect_opts):
 
     """
     Load the IDs
     """
-    test_genres = ["Rock", "Pop", "Country", "Blues", "Jazz", "Rap"]
-    ground_truth = {}
+    categories = ["Rock", "Pop", "Country", "Blues", "Jazz", "Rap"]
+    
+    train_truth = []
+    test_truth = []
 
-    train_ID = []
-    test_ID = []
+    train_IDs = []
+    test_IDs = []
 
     with open("msd_tagtraum_cd2c.cls") as f:
         i = 0
         for line in f:
-            track, genre = line.split()
-            if genre not in test_genres:
+            track, category = line.split()
+            if category not in categories:
                 continue
             if i < 100:
-                ground_truth[track] = genre
+                train_truth.append(category)
                 train_ID.append(track)
             elif i < 125:
-                ground_truth[track] = genre
+                test_truth.append(category)
                 test_ID.append(track)
             else:
                 break
             i += 1
 
-    # read training data
-
-    if classifier_opts == naivebayes:
+    # vectorize train and test
+    train_matrix, test_matrix = vectorization.vectorize(train_IDs, test_IDs,
+                                                        vect_opts)
+    
+    # classify based on classifier_opts
+    if classifier_opts == "naive_bayes":
+        predicted_test_categories = naive_bayes_classifier(train_matrix,
+                                                           test_matrix,
+                                                           train_truth)
+    elif classifier_opts == "svm":
         pass
-    else:  # Vectorization
-        train_matrix, test_matrix = vectorization.vectorize(train_ID, test_ID, vect_opts)
-        # classification method (cosine sim, machine learning, etc)
+
+    else:
+        print("Unrecognized classification")
+        sys.exit(1)
+
+    return predicted_test_categories, test_truth
 
 
-def test():
+def evaluation(predicted_test_categories, test_truth):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
