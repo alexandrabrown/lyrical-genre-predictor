e05b9c499c05f67f0f72c39b95b88624563bd5bd
Harry Zhang <zharry@umich.edu>
2017-03-28 22:10:47 -0400

    Added binary vectorizer

--- vectorization.py
+++ vectorization.py
@@ -1,18 +1,20 @@
 import tf_idf
 import count_vec
+import binary_vec
 import database
 
-def vectorization(train_IDs, test_IDs, vect_opts):
+def vectorization(train_IDs, test_IDs, vect_opts, output_matrix="dense"):
     train_lyrics = []
     test_lyrics = []
     train_lyrics = database.get_track_list(train_IDs)
     test_lyrics = database.get_track_list(test_IDs)
 
-
     if vect_opts == "tf_idf":
-        return tf_idf.tf_idf_vectorize(train_lyrics, test_lyrics, "dense")
-	else vect_opts == "count":
-		return count_vec.count_vectorize(train_lyrics, test_lyrics, "dense")
+        return tf_idf.tf_idf_vectorize(train_lyrics, test_lyrics, output_matrix)
+	elif vect_opts == "count":
+		return count_vec.count_vectorize(train_lyrics, test_lyrics, output_matrix)
+	elif vect_opts == "binary":
+		return binary_vec.binary_vectorize(train_lyrics, test_lyrics, output_matrix)
     else:
         print("Unrecognized vectorization")
         sys.exit(1)
